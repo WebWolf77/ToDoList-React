98a3ceefe3e989c6b6e3258a8b43615def2818a7
WebWolf77 <tommyecom888@gmail.com>
2024-11-10 22:46:01 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,53 +4,61 @@ import TaskItem from "./components/TaskItem";
 import Stats from "./components/Stats";
 
 function App() {
+  // State to hold the list of tasks
   const [toDoList, setToDoList] = useState([]);
 
+  // Function to add a new task to the list
   const addTask = (taskName) => {
     const newTask = { taskName, checked: false };
-    setToDoList([...toDoList, newTask])
+    setToDoList([...toDoList, newTask]);
   };
 
-    function deleteTask(deleteTaskName) {
-      setToDoList(toDoList.filter((task) => task.taskName !== deleteTaskName));
-    }
+  // Function to delete a task from the list
+  function deleteTask(deleteTaskName) {
+    setToDoList(toDoList.filter((task) => task.taskName !== deleteTaskName));
+  }
 
-    function toggleCheck(taskName){
-      setToDoList((prevToDoList) =>
+  // Function to toggle the 'checked' status of a task
+  function toggleCheck(taskName) {
+    setToDoList((prevToDoList) =>
       prevToDoList.map((task) =>
-        task.taskName === taskName ? {...task, checked: !task.checked } : task,
-      ),
+        task.taskName === taskName ? { ...task, checked: !task.checked } : task
+      )
     );
-    }
+  }
 
-  console.log(toDoList);
   return (
-  <>
-  <div className="container">
-    <h1>Task Master</h1>
+    <>
+      <div className="container">
+        <h1>Task Master</h1>
 
-    <TaskInput addTask={addTask} />
-    
+        {/* Input component to add new tasks */}
+        <TaskInput addTask={addTask} />
 
-    <div className="toDoList">
-      <span>To Do :</span>
-      <ul className="list-items">
-        {toDoList.map((task, key) => (
-        <TaskItem task={task} key = {key}
-        deleteTask={deleteTask}
-        toggleCheck={toggleCheck}/>
-        ))}
-      </ul>
-  
+        <div className="toDoList">
+          <span>To Do :</span>
+          <ul className="list-items">
+            {toDoList.map((task, key) => (
+              <TaskItem
+                task={task}
+                key={key}
+                deleteTask={deleteTask}
+                toggleCheck={toggleCheck}
+              />
+            ))}
+          </ul>
 
-        {toDoList.length === 0 ? (
-          <p className="notify">You are done!</p>
-        ) : null}
-    </div>
-  </div>
-  <Stats toDoList={toDoList} />
-  </>
+          {/* Notification message when all tasks are completed */}
+          {toDoList.length === 0 ? (
+            <p className="notify">You are done!</p>
+          ) : null}
+        </div>
+      </div>
+      
+      {/* Component to display statistics of the to-do list */}
+      <Stats toDoList={toDoList} />
+    </>
   );
-};
+}
 
 export default App;
